40ddf21571bd314f9dcbe4cac29065589f5fe011
FelipeCauan <felipecauansf@gmail.com>
2026-04-02 21:30:04 -0300

    Add files via upload

---
+++ Restaurante/src/Main.java
@@ -0,0 +1,12 @@
+import model.dao.*;
+import model.bean.*;
+import view.*;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuAdm view = new MenuAdm();
+        view.setVisible(true);
+    }
+}
